8be0e2391b2dd75b0566c283cd58a5e2049eb317
Cococatty <carinauc@outlook.com>
2019-02-06 23:05:53 +1300

    fixing files

--- Consolidating Data.R
+++ Consolidating Data.R
@@ -1,110 +1,51 @@
-getwd()
+rm(list = ls(all = TRUE))
+
+# getwd()
 
 library(data.table)
 
-list.files()
-list.files("./Inputs")
+source("Main Functions.R")
 
-##    REAWD ALL COLUMNS IN FILE OF DATA TYPE STRING
-ccData <- data.table(read.csv("Inputs/2018-4/AXXXX_XXXX_XXXX_6144-10Apr18.csv", colClasses = c("character"))
-                     , stringsAsFactors = F)
-
-##  TAKE OUT SPECIAL CHARACTER OF "." IN COLUMN NAMES
-names(ccData) <- gsub(pattern = "[.]", x = names(ccData), "")
-
-##  TO DO: If col name like "date" then do the conversion
-ccData[, ":=" ( ProcessDate = as.Date(TransactionDate, "%d/%m/%Y")
-              , TransactionDate = as.Date(TransactionDate, "%d/%m/%Y")
-              , Amount = as.numeric(Amount)
-       )]
-
-
-# dateCols <- c("ProcessDate", "TransactionDate")
-
-
-# dailyData <- data.table(read.csv("Inputs/2018-4/A0315920567389000-10Apr18.csv"))
-# homeLoanData <- data.table(read.csv("Inputs/2018-4/A0315920567389091-10Apr18.csv"))
+dtConslidated <<- data.table( Reference = character()
+                      , OtherParty = character()
+                      , Amount = numeric()
+                      , AcctType = character()
+                      , Note = character()
+                      # , ID = integer()
+                      , TransactionDate = ymd()
+                      , TransWDay = integer()
+                      , TransDay = integer()
+                      , TransMonth = integer()
+                      , TransYear = integer()
+)
 
 
 
+dtColStructure <<- data.table(
+  AcctType = c("CC", "Daily")# , "Loan"
+  , Reference = c("CreditPlanName", "AnalysisCode")
+  , OtherParty = c("OtherParty", "OtherParty-Particulars")
+  , Amount = c("Amount", "Amount")
+  , Note = c("ForeignDetails", "Description")
+  , TransDate = c("TransactionDate", "Date")
+) 
 
-comDT <<- data.table( FullDate = as.Date(character())
-                    , Amount = numeric()
-                    , OtherParty = character()
-                    , Reference = character()
-                    , AcctType = character()
-                    , Note = character()
-                    , ID = integer()
-                    , Day = integer()
-                    , Month = integer()
-                    , Year = integer()
-                    )
-
-
-
-lastID <- ifelse(nrow(comDT) == 0, 1 )
-
-#########         Env Setup Start         #########
-newData <- ccData
-acctType <- "CC"
-
-#########         Env Setup End         #########
-
-basicProecss <- function(newData, acctType) {
-  
-  newDetails <- newData[ , .( TransactionDate
-                              , Amount
-                              , OtherParty
-                              , CreditPlanName
-                              , acctType
-                              , ForeignDetails
-                            )]
-  #########         Env Setup Start         #########
-  attach(newDetails)
-  detach(newDetails)
-  #########         Env Setup End         #########
-  
-  newDetails[, ":=" (
-    ID = seq(from = lastID, by = 1, to = nrow(newDetails))
-    , Date = mday(TransactionDate)
-    , Month = month(TransactionDate)
-    , Year = year(TransactionDate)
-  )]
-  
-  
-  # temp <- rbind(comDT, newDetails, fill = TRUE)
-  (temp <- rbind(comDT, newDetails, fill = FALSE))
-  )
-    
-
-}
-
-
-############            LAB            ############
-rm(list = ls())
-rm(newDetails)
-
-class(ccData$Amount)
-names(ccData)
-class(ccData)
-class(newDetails)
-
-View(ccData)
-View(dailyData)
-View(homeLoanData)
-View(comDT)
-
-head(ccData)
-head(dailyData)
-head(homeLoanData)
-
-# attach(ccData)
+# MonthsToProcess <<- c("2018-4", "2018-5")
+MonthsToProcess <<- "2018-4"
 
 
 
-?merge
+processData()
 
-class(as.Date(ccData$ProcessDate, format ="%d/%m/%Y"))
-class(ccData$ProcessDate)
-class(ccData$TransactionDate)
 
+
+####################          CHOOSING OBJECTS TO BE SAVED AS RDATA - BEGINS           ####################
+
+objToSearchNSave <- c("data", "colsList", "dt")
+saveToRData(objToSearchNSave)
+
+####################          CHOOSING OBJECTS TO BE SAVED AS RDATA - ENDS           ####################
+
+
+
+loadRData()
