b0d1a105b3ed337ab64c812fe3068b4f98972777
Cococatty <carinauc@outlook.com>
2019-02-09 23:15:51 +1300

    more pkgs

---
+++ Archived.R
@@ -0,0 +1,12 @@
+
+
+############################                PACKAGES AND LIBRARY                ############################
+# install.packages(pkgs = "pbkrtest", lib = defaultLibraryLoc, repos = getOption("repos")
+#                  , contriburl = "https://cran.r-project.org/src/contrib/pbkrtest_0.4-7.tar.gz"
+#                  , dependencies = TRUE
+#                  , verbose = TRUE)
+#                  
+
+
+install.packages("C:/Projects/Neptune/R pkg/openxlsx_4.1.0.zip", repos = NULL, type = "win.binary", lib=defaultLibraryLoc)
+install.packages("C:/Projects/Neptune/R pkg/pbkrtest_0.4-7.zip", repos = NULL, type = "win.binary", lib=defaultLibraryLoc)
--- Network Setup.R
+++ Network Setup.R
@@ -18,10 +18,8 @@ getOption("repos")
 
 install.packages(c("assertr", "data.table", "lubridate", "sqldf", "XLConnect"))
 
+install.packages(c("data.table", "lubridate"))
 
-
-install.packages("C:/Projects/Neptune/R pkg/openxlsx_4.1.0.zip", repos = NULL, type = "win.binary", lib=defaultLibraryLoc)
-install.packages("C:/Projects/Neptune/R pkg/pbkrtest_0.4-7.zip", repos = NULL, type = "win.binary", lib=defaultLibraryLoc)
 
 
 ############        PACKRAT
@@ -30,12 +28,5 @@ install.packages("devtools")
 devtools::install_github("rstudio/packrat")
 
 packrat::init("C:/Projects/Neptune")
-# packrat::init()
 
 
-
-
-# install.packages(pkgs = "pbkrtest", lib = defaultLibraryLoc, repos = getOption("repos")
-#                  , contriburl = "https://cran.r-project.org/src/contrib/pbkrtest_0.4-7.tar.gz"
-#                  , dependencies = TRUE
-#                  , verbose = TRUE)
