2f60c3c90a34328eba004dba9bc9650792603434
Cococatty <carinauc@outlook.com>
2020-07-04 02:05:15 +1200

    cleanup
    move from R to Python

--- archived/Recycled Code.R
+++
@@ -1,3 +0,0 @@
-######################                  CALCULATIONS
-# data.table(tapply(abs(as.numeric(dtReportData$Amount)), dtReportData$ExpCategory, FUN=sum))
-# data.table(x = 1, y = 2)
--- test script.R
+++
@@ -1,7 +0,0 @@
-drStart <- ymd("20190501")
-drEnd <- ymd("20190801")
-
-i <- 1
-
-
-dtIncome <- dtFormattedRawData[ BalType == "Credit" & Note %in% c("Salary", "McCulloch")]
