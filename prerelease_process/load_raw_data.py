2d172a81309adb959d24391c24fe76ea5dbd5054
Cococatty <carinauc@outlook.com>
2020-07-06 09:33:49 +1200

    in progress - renamed dfs

--- prerelease_process/load_raw_data.py
+++ prerelease_process/load_raw_data.py
@@ -5,41 +5,43 @@ sys.path.append(os.path.abspath('C:\\Projects\\NeptuneX\\'))
 
 from project_imports import *
 
-# from importlib import *
+from importlib import *
 # project_imports = importlib.reload(project_imports)
+init_metadata = importlib.reload(init_metadata)
 
 
 #####################---------LOAD DATA---------##################### 
 load_file = glob.glob(init_metadata.file_path_data.format(init_metadata.file_key_CC,'*'))
-df_cc = pd.read_csv(load_file[0])
+df_cc_source = pd.read_csv(load_file[0])
 
 load_file = glob.glob(init_metadata.file_path_data.format(init_metadata.file_key_daily,'*'))
-df_daily = pd.read_csv(load_file[0])
+df_daily_source = pd.read_csv(load_file[0])
 # load_file
 
 
 #####################---------BASIC MANIPULATION---------##################### 
 
 # Assign Unique ID
-df_daily['df_id'] = df_daily.index
-df_cc['df_id'] = df_cc.index
+df_daily_source['source_id'] = df_daily_source.index
+df_cc_source['source_id'] = df_cc_source.index
 
 # set acct type
-df_daily['trans_acct'] = 'Daily'
-df_cc['trans_acct'] = 'CC'
+df_daily_source['source_acct'] = 'Daily'
+df_cc_source['source_acct'] = 'CC'
 
 # rename fields
-df_daily = df_daily.rename(columns={'Date':'trans_date', 'Amount':'trans_amount', 'Description':'trans_type', 'Particulars':'analysis_code'})
-df_cc = df_cc.rename(columns={'Transaction Date':'trans_date', 'Amount':'trans_amount', 
-'Other Party':'other_party', 'Credit Plan Name':'trans_type', 'Analysis Code':'analysis_code', 'City':'foreign_city','Country Code':'foreign_country'})
+df_daily_source = df_daily_source.rename(columns={'Date':'trans_date', 'Amount':'trans_amount', 'Description':'trans_category', 'Particulars':'analysis_code'})
+df_cc_source = df_cc_source.rename(columns={'Transaction Date':'trans_date', 'Amount':'trans_amount', 
+'Other Party':'other_party', 'Credit Plan Name':'trans_category', 'Analysis Code':'analysis_code', 'City':'foreign_city','Country Code':'foreign_country'})
 
-df_cc.columns = (df_cc.columns.str.replace(' ', '_')).str.lower()
-df_daily.columns = (df_daily.columns.str.replace(' ', '_')).str.lower()
+df_cc_source.columns = (df_cc_source.columns.str.replace(' ', '_')).str.lower()
+df_daily_source.columns = (df_daily_source.columns.str.replace(' ', '_')).str.lower()
 
 print('Finished BASIC MANIPULATION')
 
 
 #####################---------FOREIGN DETAILS EXTRACTION---------##################### 
+
 ########    EXTRACT BY REGEXP
 ## CC
 # foreign_amount	Foreign Details
@@ -48,15 +50,24 @@ break_foreign_details = lambda x: x.str.extract(
     r'(?P<foreign_date>\d+/\d+/\d+)\s+(?P<foreign_amt>\d+.\d+)\s+(?P<foreign_currency>\w+)\s+(?P<foreign_process>[\w\s]+)\$\s+(?P<foreign_fee>\d+.\d+)\s+(?P<foreign_process_currency>\w+)',
     expand=True)
 
-# df_foreign_details = df_cc.apply(lambda x: break_foreign_details(df_cc.loc[:,'foreign_details']), axis=1)
-col_detail = df_cc.loc[:, 'foreign_details'].astype('str')
+# df_foreign_details = df_cc_source.apply(lambda x: break_foreign_details(df_cc_source.loc[:,'foreign_details']), axis=1)
+col_detail = df_cc_source.loc[:, 'foreign_details'].astype('str')
 df_foreign_details = break_foreign_details(col_detail)
 
-df_foreign_details['df_id'] = pd.Series(range(0, len(df_foreign_details)))
+df_foreign_details['source_id'] = pd.Series(range(0, len(df_foreign_details)))
 
-df_cc = pd.merge(df_cc, df_foreign_details, how='left', on='df_id')
+df_cc_source = pd.merge(df_cc_source, df_foreign_details, how='left', on='source_id')
 
 
+df_cc_source_cols = [
+    'source_id', 'source_acct', 
+    'trans_date', 'trans_amount', 'other_party', 'trans_category',
+    'foreign_details', 'foreign_city', 'foreign_country',
+    'foreign_date', 'foreign_amt',
+    'foreign_currency', 'foreign_process', 'foreign_fee',
+    'foreign_process_currency',]
+
+df_cc = pd.DataFrame(df_cc_source, columns=df_cc_source_cols)
 
 ########    EXTRACT BY NLP
 
@@ -68,7 +79,7 @@ df_cc = pd.merge(df_cc, df_foreign_details, how='left', on='df_id')
 
 
 ## Testing setup
-# col_foreign_details = df_cc[df_cc.Foreign_Details.notnull()]['Foreign_Details']
+# col_foreign_details = df_cc_source[df_cc_source.Foreign_Details.notnull()]['Foreign_Details']
 # print(col_foreign_details)
 # col_foreign_details.to_csv('foreign_detils_col.csv', columns=['id', 'item'])
 # # 
@@ -97,15 +108,16 @@ df_cc = pd.merge(df_cc, df_foreign_details, how='left', on='df_id')
 ##---------AFTER THE CONSOLIDATED DF IS BUILT---------
 
 #####################---------RANDOMIZE VALUES---------##################### 
-generate_randomizers(
-    items_to_randomize=init_metadata.list_item_toRandom,
-    str_header='item, rand_perc',    
-    export_result=True, 
-    file_name='meta_randomization')
+def to_complete():
+    main_functions.generate_randomizers(
+        items_to_randomize=init_metadata.list_item_toRandom,
+        str_header='item, rand_perc',    
+        export_result=True, 
+        file_name='meta_randomization')
 
-randomize_values(df_target=df_final)
+    main_functions.randomize_values(df_target=df_final)
 
-print('Finished RANDOMIZING VALUES')
+    print('Finished RANDOMIZING VALUES')
 
 #####################---------REMOVE SENSITIVE INFORMATION---------##################### 
 ## Names
